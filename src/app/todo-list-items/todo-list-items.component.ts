a852bc557d6a405ebd7e0f65f417fa7ed490ad5c
Farah <Farahalhartani4@gmail.com>
2024-08-19 17:30:13 +0300

    Styling todo list items component

--- src/app/todo-list-items/todo-list-items.component.ts
+++ src/app/todo-list-items/todo-list-items.component.ts
@@ -3,11 +3,11 @@ import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatCheckboxModule} from '@angular/material/checkbox';
 @Component({
   selector: 'app-todo-list-items',
   standalone: true,
-  imports: [MatButtonModule,MatListModule, MatDividerModule,MatIconModule],
+  imports: [MatButtonModule, MatListModule, MatDividerModule, MatIconModule, MatCheckboxModule],
   templateUrl: './todo-list-items.component.html',
   styleUrl: './todo-list-items.component.css'
 })
