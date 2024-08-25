754adbfb0d333e24c967c4e912d12cdec4c156d1
Farah <Farahalhartani4@gmail.com>
2024-08-25 12:42:01 +0300

    Added Edit and Delete Functionality with ngModel in TodoList Item Component

--- src/app/todo-list-items/todo-list-items.component.ts
+++ src/app/todo-list-items/todo-list-items.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { Component, Input } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-todo-list-items',
   standalone: true,
-  imports: [MatButtonModule, MatListModule, MatDividerModule, MatIconModule, MatCheckboxModule],
+  imports: [MatButtonModule, MatListModule, MatDividerModule, MatIconModule, MatCheckboxModule, FormsModule,
+    CommonModule
+  ],
   templateUrl: './todo-list-items.component.html',
   styleUrl: './todo-list-items.component.css'
 })
 export class TodoListItemsComponent {
+  @Input() items: {text:string; isEditable?:boolean; completed?:boolean}[] = [];
 
-}
+ 
+
+  deleteItem(task: {text:string}) {
+    this.items = this.items.filter(item => item.text !== task.text);
+  }
+
+  editItem(task: {isEditable?:boolean}){
+    if (task.isEditable) {
+      
+      task.isEditable = false;
+    } else {
+      
+      task.isEditable = true;
+    }
+  }
+    
+  }
